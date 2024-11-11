445ec7839f454956a81554373c2dcc64aac8d00b
shakrui21 <shakruiiii111@gmail.com>
2024-11-11 10:47:50 +0000

    Update ProductSubtotalView.sql

--- ProductSubtotalView.sql
+++ ProductSubtotalView.sql
@@ -3,6 +3,6 @@ SELECT
     Product_number,
     Product_name,
     Price AS Base_Price,
-    Price * 1.2 AS Subtotal  -- Adds £5 shipping and applies 20% VAT
+    Price * 1.2 AS Subtotal  -- applies 20% VAT
 FROM 
     PRODUCT;
